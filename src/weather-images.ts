52ca7316a27eacca863d537a864bd1936d1cdf83
Thyraz <tobias.wiedenmann@me.com>
2025-06-07 14:22:14 +0200

    Added daily min/max bars chart

---
+++ src/weather-images.ts
@@ -0,0 +1,27 @@
+import sunnyConditionImage from './img/sunny.jpg';
+import pouringConditionImage from './img/pouring.jpg';
+import partlyCloudyConditionImage from './img/partly-cloudy.jpg';
+import fogConditionImage from './img/fog.jpg';
+import hailConditionImage from './img/hail.jpg';
+import lightningRainyConditionImage from './img/lightning-rainy.jpg';
+import lightningConditionImage from './img/lightning.jpg';
+import rainyConditionImage from './img/rainy.jpg';
+import snowyRainyConditionImage from './img/snowy-rainy.jpg';
+import snowyConditionImage from './img/snowy.jpg';
+import windyVariantConditionImage from './img/windy-variant.jpg';
+import windyConditionImage from './img/windy.jpg';
+
+export const WeatherImages = {
+  pouring: pouringConditionImage,
+  sunny: sunnyConditionImage,
+  partlycloudy: partlyCloudyConditionImage,
+  fog: fogConditionImage,
+  hail: hailConditionImage,
+  lightningrainy: lightningRainyConditionImage,
+  lightning: lightningConditionImage,
+  rainy: rainyConditionImage,
+  snowyrainy: snowyRainyConditionImage,
+  snowy: snowyConditionImage,
+  windyvariant: windyVariantConditionImage,
+  windy: windyConditionImage
+};
